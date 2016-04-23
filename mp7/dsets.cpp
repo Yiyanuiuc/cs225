bb018da4c9cbd3ec4898c909c01711dbd090b6e4
qilezhi2 <suyie@users.noreply.github.com>
2016-04-23 01:01:18 +0000

    mp7.1

--- mp7/dsets.cpp
+++ mp7/dsets.cpp
@@ -1,1 +1,48 @@
 /* Your code here! */
+
+#include <vector>
+#include "dsets.h"
+
+using namespace std;
+
+/** 
+ * Creates n unconnected root nodes at the end of the vector. 
+ * Parameters num The number of nodes to create
+ */
+void DisjointSets::addelements (int num) {
+	for (size_t i=0; i<(size_t)num; i++) {
+		elems.push_back(-1);
+	}
+}
+
+/** 
+ * This function should compress paths and works as described in lecture. 
+ * Returns the index of the root of the up-tree in which the parameter element resides.
+*/
+int DisjointSets::find (int elem) {
+	if (elem<0 || elem>=(int)elems.size()) return -1;
+	// base case: reach root
+	else if (elems[elem]<0) return elem;
+	// recursive case
+	else return elems[elem] = find(elems[elem]);
+}
+
+/** 
+ * This function should be implemented as union-by-size. 
+ * Parameters 
+ * a Index of the first element to union
+ * b Index of the second element to union
+ */
+void DisjointSets::setunion (int a, int b) {
+	int root1 = find(a);
+	int root2 = find(b);
+	if (root1==root2) return;
+	int sizeA = 0;
+	int sizeB = 0;
+	for (size_t i=0; i<elems.size(); i++) {
+		if (find(i)==root1) sizeA++;
+		else if (find(i)==root2) sizeB++;
+	}
+	if (sizeA<sizeB) elems[root1] = root2;
+	else elems[root2] = root1;
+}
